fc09542e3f70c073340a021eff7782cee0c97b93
Spencer Lindemuth <lindemut@gmail.com>
2019-03-22 17:14:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,9 @@
 # Write your code here.
 def take_a_number(line, name)
   puts "Welcome, #{name}. You are number #{line.length + 1} in line."
-  return line << name
+  line << name
 def line(array)
   if !array.length
     puts "The line is currently:"
   end
 end
-
-
