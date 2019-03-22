a7531948984ed52e93ca655745c21f209ef6f609
Spencer Lindemuth <lindemut@gmail.com>
2019-03-22 17:37:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def line(array)
  if array.length == 0
    puts "The line is currently empty."
  else
-  array.each_index do |index, name|
+  array.each_index do |index|
     if (index < array.length)
       base += "#{index + 1}. #{array[index]}, "
     else
