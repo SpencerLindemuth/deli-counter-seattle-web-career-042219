087a4174dd63620fd6a1a8ce4907f632e074e207
Spencer Lindemuth <lindemut@gmail.com>
2019-03-22 17:17:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,13 @@ def take_a_number(line, name)
   puts "Welcome, #{name}. You are number #{line.length + 1} in line."
   line << name
 end
+
+def now_serving(line)
+  if !line.length
+    puts "Currently serving #{line.first}"
+    return line.shift!
+  else
+    puts "The line is currently empty"
 def line(array)
   if !array.length
     puts "The line is currently:"
